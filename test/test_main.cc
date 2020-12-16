dbbeb140c17848dd5b2d18258ec399f66103dac2
Alex <alexkrapivin@gmx.com>
2020-12-16 10:57:51 +0100

    Add some files

---
+++ test/test_main.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "test_inc.h"
+
+int main(void)
+{
+    // Defect example:
+    // int a = 0;
+    // int b = 1 / a;
+    printf("test_main: Hello World!\n");
+    return 0;
+}
